6445befe4c3f770bf8d8f5ff8a9efd0e7d9b06c2
mparajuli <mparajuli@stu.jsu.edu>
2020-01-28 15:31:40 -0600

    Test Commit

--- src/main/java/edu/jsu/mcis/HelloWorld.java
+++ src/main/java/edu/jsu/mcis/HelloWorld.java
@@ -11,9 +11,11 @@ public class HelloWorld {
     }
     
     public static void main(String[] args) {
+
         HelloWorld h = new HelloWorld();
         System.out.println(h.showIt());
         System.out.println(h.reverseIt());
+
     }
 
 }
